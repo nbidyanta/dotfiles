c2c356853f2c1d9236440229a3d01ba01df532fb
Nilangshu Bidyanta <nb0dy@dev.nb0dy.linux>
2017-06-13 02:52:59 +0000

    Added pkg-config as prerequisite; Bugfix: Return values are expressed $? and not the result of $(...); Bugfix: POSIX standard '[...]' arguments need to be quoted; Bugfix: Added missing breaks; Improved formatting;

--- dsetup.sh
+++ dsetup.sh
@@ -3,7 +3,7 @@
 # Author: Nilangshu Bidyanta
 
 echo "Environment setup"
-echo "Prereqeuisites: git, vim, curl, autotools, an internet connection"
+echo "Prereqeuisites: git, vim, curl, autotools, pkg-config, an internet connection"
 while true; do
 	read -p "Do you wish to initiate the setup procedure? [y/n]: " yn
 	case $yn in
@@ -13,28 +13,28 @@ while true; do
 	esac
 done
 
-vi_mode="set -o vi"
-dircolor_entry="eval $(dircolors -b ~/.dircolors.256dark)"
+vi_mode_entry="set -o vi"
+dircolor_entry="eval \$(dircolors -b ~/.dircolors.256dark)"
 alias_entry="alias cdw='cd $HOME/prj'"
 
 echo "Creating project directory"
-mkdir $HOME/prj
+mkdir -p $HOME/prj
 
 echo "Adding lines to .bashrc"
-ret=$(grep -s "$vi_mode_entry" $HOME/.bashrc)
-if [ $ret -eq 1 ]; then
+grep -s "$vi_mode_entry" $HOME/.bashrc
+if [ "$?" -eq "1" ]; then
 	echo -e "\tEnabling Vi mode in bash"
 	echo "$vi_mode_entry" >> $HOME/.bashrc
 fi
 
-ret=$(grep -s "$dircolor_entry" $HOME/.bashrc)
-if [ $ret -eq 1 ]; then
+grep -s "$dircolor_entry" $HOME/.bashrc
+if [ "$?" -eq "1" ]; then
 	echo -e "\tAdding dircolor entry"
 	echo "$dircolor_entry" >> $HOME/.bashrc
 fi
 
-ret=$(grep -s "$alias_entry" $HOME/.bashrc)
-if [ $ret -eq 1 ]; then
+grep -s "$alias_entry" $HOME/.bashrc
+if [ "$?" -eq "1" ]; then
 	echo -e "\tCreating an alias for changing into work directory"
 	echo "$alias_entry" >> $HOME/.bashrc
 fi
@@ -49,85 +49,86 @@ cp dircolors.256dark $HOME/.dircolors.256dark
 
 echo "Populating vim plugins"
 if [ ! -e "$HOME/.vim/autoload/pathogen.vim" ]; then
-	echo -e "\tInstalling pathogen.vim"
+	echo -e "\n\tInstalling pathogen.vim"
 	mkdir -p $HOME/.vim/autoload/ $HOME/.vim/bundle && \
 		curl -LSso ~/.vim/autoload/pathogen.vim \
 		https://tpo.pe/pathogen.vim
 else
-	echo -e "Pathogen already installed"
+	echo -e "\n\tPathogen already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/a.vim" ]; then
-	echo -e "\tInstallingn A.vim"
+	echo -e "\n\tInstallingn A.vim"
 	git clone https://github.com/vim-scripts/a.vim.git \
 		$HOME/.vim/bundle/a.vim
 else
-	echo -e "\tA.vim already installed"
+	echo -e "\n\tA.vim already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/ctrlp.vim" ]; then
-	echo -e "\tInstalling CtrlP"
+	echo -e "\n\tInstalling CtrlP"
 	git clone https://github.com/ctrlpvim/ctrlp.vim.git \
 		$HOME/.vim/bundle/ctrlp.vim
 else
-	echo -e "\tCtrlP already installed"
+	echo -e "\n\tCtrlP already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/vim-autotag" ]; then
-	echo -e "\tInstalling vim-autotag"
+	echo -e "\n\tInstalling vim-autotag"
 	git clone https://github.com/craigemery/vim-autotag.git \
 		$HOME/.vim/bundle/vim-autotag
 else
-	echo -e "\tvim-autotag already installed"
+	echo -e "\n\tvim-autotag already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/vim-colors-solarized" ]; then
-	echo -e "\tInstalling solarized theme"
+	echo -e "\n\tInstalling solarized theme"
 	git clone https://github.com/altercation/vim-colors-solarized \
 		$HOME/.vim/bundle/vim-colors-solarized
 else
-	echo -e "\tSolarized theme already installed"
+	echo -e "\n\tSolarized theme already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/syntastic" ]; then
-	echo -e "\tInstalling Syntastic"
+	echo -e "\n\tInstalling Syntastic"
 	git clone --depth=1 https://github.com/vim-syntastic/syntastic.git \
 		$HOME/.vim/bundle/syntastic
 else
-	echo -e "\tSyntasic already installed"
+	echo -e "\n\tSyntasic already installed"
 fi
 
 if [ ! -d "$HOME/.vim/bundle/rust.vim" ]; then
-	echo -e "\tInstalling rust.vim"
+	echo -e "\n\tInstalling rust.vim"
 	git clone --depth=1 https://github.com/rust-lang/rust.vim.git \
 		$HOME/.vim/bundle/rust.vim
-	echo "Don't forget to install rustfmt using:"
-	echo "cargo install rustfmt"
+	echo -e "\n\tDon't forget to install rustfmt using:"
+	echo -e "\tcargo install rustfmt\n"
 else
-	echo -e "\trust.vim already installed"
+	echo -e "\n\trust.vim already installed"
 fi
 
 if [ ! -d "$HOME/.cargo" ]; then
 	while true; do
 		read -p "Do you wish to install rust? [y/n]: " yn
 		case $yn in
-			[Yy]* ) curl https://sh.rustup.rs -sSf | sh;;
+			[Yy]* ) curl https://sh.rustup.rs -sSf | sh; break;;
 			[Nn]* ) break;;
 			* ) ;;
 		esac
 	done
 fi
 
-ret=$(which ctags > /dev/null)
-if [ $ret -eq 1 ]; then
+which ctags > /dev/null
+if [ "$?" -eq "1" ]; then
 	while true; do
-		read -p "Do you wish to install rust? [y/n]: " yn
+		read -p "Do you wish to install ctags? [y/n]: " yn
 		case $yn in
 			[Yy]*)
 				git clone https://github.com/universal-ctags/ctags.git \
 					/tmp/ctags
 				cd /tmp/ctags && ./autogen.sh && ./configure && \
 					make && sudo make install
+				break
 				;;
 			[Nn]* ) break;;
 			* ) ;;
